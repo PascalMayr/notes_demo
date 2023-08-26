b2aa3c907368a3a9af8fd07e0204122defffbed1
Pascal Mayr <pascalmayr2@gmail.com>
2023-08-26 18:33:17 +0200

    setup swagger for API documentation

--- server/index.ts
+++ server/index.ts
@@ -1,4 +1,6 @@
 import express from 'express'
+import swaggerUi from 'swagger-ui-express'
+import swaggerSpecs from './utils/swagger.js'
 import cors from 'cors'
 import chalk from 'chalk'
 import notesRouter from './routes/api/v1/note/index.js'
@@ -9,6 +11,12 @@ const port = process.env.port || 3000
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
+// API docs
+app.use(
+  "/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerSpecs, { explorer: true })
+)
 app.listen(port, () => {
   console.log(chalk.green(`Server listening at http://localhost:${port}`));
 })
---
+++ server/utils/swagger.ts
@@ -0,0 +1,29 @@
+import swaggerJSDoc from 'swagger-jsdoc'
+
+/*
+* @description - Swagger definition
+*/
+const options = {
+  definition: {
+    openapi: "3.1.0",
+    info: {
+      title: "Demo Notes API Docs",
+      version: "0.1.0",
+      description:
+        "This is a simple CRUD API application made with Express and documented with Swagger.",
+      contact: {
+        name: "Pascal Mayr",
+        url: "https://pascalmayr.com",
+        email: "mail@pascalmayr.com`",
+      },
+    },
+    servers: [
+      {
+        url: "http://localhost:3000",
+      },
+    ],
+  },
+  apis: [""],
+};
+
+export default swaggerJSDoc(options);
