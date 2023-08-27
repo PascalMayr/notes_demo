ad9ea070b8079646a970f84ecf1810b4361f1cd3
Pascal Mayr <pascalmayr2@gmail.com>
2023-08-27 18:43:47 +0200

    create client react app

---
+++ client/README.md
@@ -0,0 +1,11 @@
+# DEMO Notes App
+
+## Getting started
+1. Run
+```
+npm i
+```
+
+2. Run
+```
+npm start
---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import List from 'components/List'
+import Note from 'components/Note'
+
+function App() {
+  const { current } = useApp()
+  return (
+    <section className="containers">
+    </section>
+  )
+}
+
+export default App
