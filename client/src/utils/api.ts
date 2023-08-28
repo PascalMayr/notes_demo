a232744e6d4812d1e9c9d286c7dccebf8f0caf4a
Pascal Mayr <pascalmayr2@gmail.com>
2023-08-28 19:28:06 +0200

    create api utility

---
+++ client/src/utils/api.ts
@@ -0,0 +1,17 @@
+const API_PORT = process.env.API_PORT || 3001
+const API_URL = process.env.API_URL || `//localhost:${API_PORT}/api/v1`
+
+/**
+ * Returns a promise which resolves to the local API result.
+ *
+ * @param {string} path The local API path.
+ * @param {RequestInit} init fetch() options.
+ */
+const clientApi = async <T>(
+  path: string,
+  init: RequestInit = {}
+): Promise<T | undefined> => {
+  return await (await fetch(`${API_URL}${path}`, init)).json()
+}
+
+export default clientApi
