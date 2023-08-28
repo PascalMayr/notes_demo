e11a2644a5e256ea208442ec93e16b3c7a720977
Pascal Mayr <pascalmayr2@gmail.com>
2023-08-28 22:41:20 +0200

    add types to error handler

--- server/utils/errorHandler.ts
+++ server/utils/errorHandler.ts
@@ -1,16 +1,18 @@
 import { Prisma } from "@prisma/client"
 import chalk from "chalk"
+import { Request, Response } from "express";
 
 /*
 * Catch all errors and log
 */
-function errorHandler(error, _req, res, _next) {
+function errorHandler(error: any, res: Response, req: Request) {
   if (error instanceof Prisma.PrismaClientKnownRequestError) {
     console.log(chalk.blue('Database error'));
   }
   console.log(error.message)
   // internal server error
-  res.status(500).send({ error: error.message })
+  res.status(500)
+  res.send({ error: error.message })
 }
 
 export default errorHandler
