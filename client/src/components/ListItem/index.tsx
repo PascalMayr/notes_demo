bc1d03869a1216c0ae8761700b092575c690e934
Pascal Mayr <pascalmayr2@gmail.com>
2023-08-29 10:41:03 +0200

    show current note when clicking on list item

--- client/src/components/ListItem/index.tsx
+++ client/src/components/ListItem/index.tsx
@@ -1,13 +1,24 @@
-import { INote } from 'context/AppContext'
+import { INote, useApp } from 'context/AppContext'
 import styles from './styles.module.sass'
 import formatDate from 'utils/formatDate'
+import { useCallback } from 'react'
 
 function ListItem(note: INote) {
+  // set current when selecting on click
+  const { setCurrent, current } = useApp()
+  const handleClick = useCallback(() => setCurrent(note), [note])
   return (
-    <div className={styles.item}>
-      <span className={styles.itemTitle}>{note.title}</span>
+    <div onClick={handleClick} className={styles.item}>
+      <span
+        className={`${styles.itemTitle} ${
+          current?.id === note.id ? styles.active : ''
+        }`}
+        dangerouslySetInnerHTML={{ __html: note.title }}
+      />
       {note.updatedAt && (
-        <span className={styles.itemDate}>{formatDate(note.updatedAt)}</span>
+        <span className={styles.itemDate}>
+          {formatDate(new Date(note.updatedAt))}
+        </span>
       )}
     </div>
   )
